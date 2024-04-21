23c15f9cb9508ab9854955c02996b30c0990bcb6
HerbertKayky <herbertkayky9@gmail.com>
2024-04-20 21:54:14 -0300

    criando componente header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,35 @@
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/logo.svg";
+import { FiLogIn, FiUser } from "react-icons/fi";
 
 const Header = () => {
-    return (
-      <div>
-        <h1 className="">Componente header</h1>
-      </div>
-    )
-  }
-  
-  export default Header
+  const signed = false;
+  const loadingAuth = false;
+
+  return (
+    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4 ">
+      <header className="w-full max-w-7xl flex items-center justify-between px-4 mx-auto">
+        <Link to="/">
+          <img src={logoImg} alt="Logo do site" />
+        </Link>
+        {!loadingAuth && signed && (
+          <Link to="/dashboard">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiUser size={24} color="#000" />
+            </div>
+          </Link>
+        )}
+
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiLogIn size={24} color="#000" />
+            </div>
+          </Link>
+        )}
+      </header>
+    </div>
+  );
+};
+
+export default Header;
