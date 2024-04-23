7a796f7104c9442edf9273fadee5478d93e9453f
HerbertKayky <herbertkayky9@gmail.com>
2024-04-23 18:35:43 -0300

    criando página de login com hook form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,70 @@
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/logo.svg";
+import Container from "../../components/container";
+import Input from "../../components/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("Insira um email válido")
+    .min(1, "O campo email é obrigatório"),
+  password: z.string().min(2, "O campo senha é obrigatório"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Login = () => {
-    return (
-      <div>Login</div>
-    )
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
   }
-  
-  export default Login
+
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link className="mb-6 max-w-sm w-full" to="/">
+          <img className="w-full" src={logoImg} alt="Logo do site" />
+        </Link>
+
+        <form
+          className="bg-white max-w-xl w-full rounded-lg"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="digite seu email"
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="digite sua senha"
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button>Acessar</button>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default Login;
