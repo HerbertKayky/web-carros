d459fcc18b21067d98d4237d2d1d1d1b3587fe7b
HerbertKayky <herbertkayky9@gmail.com>
2024-04-29 00:55:52 -0300

    adicionando carros cadastrados à página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,57 @@
+import { useEffect, useState } from "react";
 import Container from "../../components/container";
 import { IoLocationOutline } from "react-icons/io5";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { Link } from "react-router-dom";
+
+interface CarProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImagesProps[];
+}
+
+interface CarImagesProps {
+  name: string;
+  uid: string;
+  url: string;
+}
 
 const Home = () => {
+  const [cars, setCars] = useState<CarProps[]>([]);
+
+  useEffect(() => {
+    function loadCars() {
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, orderBy("created", "desc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        let listcars = [] as CarProps[];
+
+        snapshot.forEach((doc) => {
+          listcars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid,
+          });
+        });
+
+        setCars(listcars);
+      });
+    }
+    loadCars();
+  }, []);
+
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -19,30 +69,36 @@ const Home = () => {
       </h1>
 
       <main className="gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="object-contain max-h-72 rounded-lg w-full hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202404/20240409/mitsubishi-lancer-2.0-16v-gasolina-4p-automatico-wmimagem12094240065.jpg?s=fill&w=1920&h=1440&q=75"
-            alt="Carro"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">Lancer Evolution</p>
+        {cars.map((car) => (
+          <Link key={car.id} to={`/car/${car.id}`}>
+            <section className="w-full bg-white rounded-lg">
+              <img
+                className="object-contain max-h-72 rounded-lg w-full hover:scale-105 transition-all"
+                src={car.images[0].url}
+                alt="Carro"
+              />
+              <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-6">Ano 2017/2018 º 34000 km</span>
-            <strong className="text-black font-medium text-xl">
-              R$ 45.000
-            </strong>
-          </div>
+              <div className="flex flex-col px-2">
+                <span className="text-zinc-700 mb-6">
+                  {car.year} | {car.km}
+                </span>
+                <strong className="text-black font-medium text-xl">
+                  {car.price}
+                </strong>
+              </div>
 
-          <div className="w-full h-px bg-slate-200 my-2"></div>
+              <div className="w-full h-px bg-slate-200 my-2"></div>
 
-          <div className="px-2 pb-2 flex flex-row items-center">
-            <span>
-              <IoLocationOutline />
-            </span>
-            <span className="text-zinc-700 px-1 ">Toritama - PE</span>
-          </div>
-        </section>
+              <div className="px-2 pb-2 flex flex-row items-center">
+                <span>
+                  <IoLocationOutline />
+                </span>
+                <span className="text-zinc-700 px-1 ">{car.city}</span>
+              </div>
+            </section>
+          </Link>
+        ))}
       </main>
     </Container>
   );
